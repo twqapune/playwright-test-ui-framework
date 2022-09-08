e53dee4a5ee288a700caeaefced24e991ad8a16b
Jateen Sharma <jateen.sharma@thoughtworks.com>
2022-09-08 13:08:42 +0530

    [Jateen] Initial Commit Order Summary Test.

---
+++ tests/order-summary.spec.ts
@@ -0,0 +1,7 @@
+import test from "@playwright/test";
+
+test.describe("Order summary", function () {
+    test("Should be able to download the order summary and validate order details from the downloaded pdf summary", async ({
+        page
+    }) => { });
+});
